ff6a690d6f246065801b476c1705bd45eab85187
NArunkumar <nelluri.arunkumar@gmail.com>
2025-09-20 10:10:20 +0530

    Adding eksctl and kubectl installation commands in user shell file

--- docker.sh
+++ docker.sh
@@ -15,3 +15,20 @@ sudo lvextend -L +10G /dev/RootVG/varVol
 
 sudo xfs_growfs /
 sudo xfs_growfs /var
+
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+sudo curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+
+eksctl version
+kubectl version
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
