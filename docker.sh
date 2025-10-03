2dba72050d1332e350c74e7a1dd39d160e85cbc4
NArunkumar <nelluri.arunkumar@gmail.com>
2025-10-03 09:51:53 +0530

    Addinng Helm installation commands

--- docker.sh
+++ docker.sh
@@ -16,13 +16,14 @@ sudo lvextend -L +10G /dev/RootVG/varVol
 sudo xfs_growfs /
 sudo xfs_growfs /var
 
-
+##Installing eksctl for managinng the eks cluster
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 sudo curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
+##Installing kubectl for interaction with eks cluster
 sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
@@ -30,7 +31,14 @@ sudo mv kubectl /usr/local/bin/kubectl
 eksctl version
 kubectl version
 
+##
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
-curl -sS https://webinstall.dev/k9s | bash #For K9s 
+##Installing the K9s UI
+sudo curl -sS https://webinstall.dev/k9s | bash #For K9s 
+
+##Installing Helm
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
